9a2c4862a0b4bd6288485c8c76f07e19b3949cee
Silenthal <415802+Silenthal@users.noreply.github.com>
2024-12-17 11:34:13 -0500

    Track how many labels are still default

--- tools/progress.py
+++ tools/progress.py
@@ -2,39 +2,24 @@
 import argparse
 import re
 from pathlib import Path
-from common import auto_int
 
 
 def main():
     parser = argparse.ArgumentParser(description="Calculate disassembly progress.")
-    parser.add_argument(
-        "-p", "--print", action="store_true", help="Only print the percentage."
-    )
-    parser.add_argument("path", type=str, help="The path to the map file used.")
+    parser.add_argument("-p", "--path", type=str, help="The path to the map file used.")
     args = parser.parse_args()
-    inPath = Path(args.path)
+    inPath = Path(args.path if args.path else "../build/mmbn.map")
     if not inPath.exists():
         exit(f"Couldn't find map file {args.path}")
 
-    covered = 0
-    pad = 0
-
+    default_count = 0
     with open(inPath, "r") as inMap:
         for line in inMap:
-            matchx = re.match(r"^ \.(\w+)\s+0x[0-9a-f]+\s+(0x[0-9a-f]+) (.+)\.o", line)
-            if matchx:
-                if matchx[1] == "text":
-                    size = auto_int(matchx[2])
-                    if "__pad__" in matchx[3]:
-                        pad += size
-                    else:
-                        covered += size
+            match_fun = re.search(r"[0-9A-Fa-f]{6}$", line)
+            if match_fun:
+                default_count += 1
 
-    total = covered + pad
-    if args.print:
-        print("{:.2f}".format(100 * covered / total))
-    else:
-        print("Disassembled: {:.2f}%".format(100 * covered / total))
+    print(f"Default label count: {default_count}")
 
 
 if __name__ == "__main__":
