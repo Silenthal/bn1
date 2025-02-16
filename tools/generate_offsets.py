7bc1cf11bb45037faa3162071cca9e2eb8bed432
Silenthal <415802+Silenthal@users.noreply.github.com>
2025-02-16 11:08:42 -0500

    Add macro for creating field objects Add offsets to attack functions

--- tools/generate_offsets.py
+++ tools/generate_offsets.py
@@ -5,6 +5,41 @@ from pycparser import parse_file
 from pycparser.ast_transforms import c_ast
 
 
+def processStruct(
+    parent: str,
+    memberPrefix: str,
+    structName: str,
+    nameDict: dict[str, dict[str, str]],
+    output: list[str],
+):
+    for memberName in nameDict[structName]:
+        typeName = nameDict[structName][memberName]
+        declMember = memberName
+        if memberPrefix != "":
+            declMember = memberPrefix + "." + memberName
+        declStructName = structName
+        if parent != "":
+            declStructName = parent
+        declMemberName = declStructName + "_" + declMember.replace(".", "_")
+        if structName == "Main":
+            output.append(
+                f'    DECLARE("Main_{memberName}", offsetof({structName}, {memberName}));'
+            )
+            output.append(
+                f'    DECLARE("{memberName}", offsetof({structName}, {memberName}));'
+            )
+        elif typeName != "":
+            output.append(
+                f'    DECLARE("{declMemberName}", offsetof({declStructName}, {declMember}));'
+            )
+            if typeName in nameDict:
+                processStruct(declStructName, declMember, typeName, nameDict, output)
+        else:
+            output.append(
+                f'    DECLARE("{declMemberName}", offsetof({declStructName}, {declMember}));'
+            )
+
+
 def main():
     parser = argparse.ArgumentParser(
         description="Creates a source file containing offsets for structs and enum values."
@@ -18,24 +53,27 @@ def main():
         exit(f"File {headFile} not found")
     ast = parse_file(headFile, True)
     declareList: list[str] = []
+    structDict: dict[str, dict[str, str]] = {}
+    enumList: list[tuple[str, int]] = []
     for node in ast.ext:
         if isinstance(node, c_ast.Decl) and isinstance(node.type, c_ast.Struct):
             structNode: c_ast.Struct = node.type
             for member in structNode.decls:
                 if not member.bitsize:
-                    declareList.append(
-                        f'    DECLARE("{structNode.name}_{member.name}", offsetof({structNode.name}, {member.name}));'
-                    )
-                    if structNode.name == "Main":
-                        declareList.append(
-                            f'    DECLARE("{member.name}", offsetof({structNode.name}, {member.name}));'
-                        )
+                    if structNode.name not in structDict:
+                        structDict[structNode.name] = {}
+                    typeName = ""
+                    if isinstance(member.type.type, c_ast.Struct):
+                        typeName = member.type.type.name
+                    structDict[structNode.name][member.name] = typeName
         elif isinstance(node, c_ast.Typedef) and isinstance(node.type.type, c_ast.Enum):
             enumNode: c_ast.Enum = node.type.type
             for member in enumNode.values:
-                declareList.append(
-                    f'    DECLARE("{member.name}", {member.value.value});'
-                )
+                enumList.append((member.name, member.value.value))
+    for name in structDict.keys():
+        processStruct("", "", name, structDict, declareList)
+    for k, v in enumList:
+        declareList.append(f'    DECLARE("{k}", {v});')
     declares = "\n".join(declareList)
     template = f"""#include <stddef.h>
 #include "mmbn.h"
